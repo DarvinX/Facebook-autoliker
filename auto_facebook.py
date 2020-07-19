17dff818be9bf027969beac0cd3463eda8be4533
DarvinX <tamalhansda16@gmail.com>
2020-07-19 11:42:41 +0530

    add more information

--- auto_facebook.py
+++ auto_facebook.py
@@ -9,6 +9,8 @@ def likeOrNot(probability=0.5):
     return randint(100) <= 100*probability
 
 if __name__ == "__main__":
+    
+    #bot = Bot(driverPath='executable path', headless=False, debug=True)
     bot = Bot()
     bot.login(C.f_username, C.f_password)
     totalLikes = 0
